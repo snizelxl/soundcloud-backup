6ba83be6495ea765ff379527e859b901889ed648
John Doe <snizelxl@tuta.io>
2017-09-01 00:13:29 +0200

    id3-tag the mp3s

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu:16.04
 
-RUN apt-get update && apt-get install -y curl jq
+RUN apt-get update && apt-get install -y curl jq id3v2
 
 COPY track.sh /usr/bin/track.sh
 COPY likes.sh /usr/bin/likes.sh
--- track.sh
+++ track.sh
@@ -7,12 +7,24 @@ CONTENT=$(curl -s -L $URL)
 TRACKID=$(echo "$CONTENT" | grep -Eo "sounds:([0-9]+)\"*" | sed -e 's/sounds://g' -e 's/\"//g' | head -n1)
 TITLE=$(echo "$CONTENT" | grep -Eo "(<title>).*(</title>)" | grep -Eo "^[^|]+" | sed s/\<title\>//g)
 # urldecode
-TITLE=$(printf '%b' "${TITLE//%/\\x}")
+TITLE=$(printf '%b' "${TITLE//%/\\x}" | sed "s/&#39;/'/g")
 # trim and remove slashes
-TITLE=$(echo "$TITLE" | sed -e 's/\//\|/g' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | xargs echo  -E)
+TITLE=$(echo "$TITLE" | sed -e 's/\//\|/g' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | xargs -0 echo -E)
+
+if [[ $TITLE == *" - "* ]]; then
+  # artist - trackname
+  TRACK=$(echo $TITLE | sed -e 's/[ -].*//g')
+  ARTIST=$(echo $TITLE | sed -e 's/^.*[ -]//g' -e 's/by.*$//g')
+else
+  # trackname by artist
+  ARTIST=$(echo $TITLE | sed -e 's/^.*by //g')
+  TRACK=$(echo $TITLE | sed -e 's/ by.*$//g')
+fi
+
+TITLE="$ARTIST - $TRACK"
 
 STREAM_URL="https://api.soundcloud.com/i1/tracks/$TRACKID/streams?client_id=$CLIENTID"
-PLAYLIST_URL=$(curl -s -L $STREAM_URL | jq '.hls_mp3_128_url' | sed s/\"//g | xargs echo -E)
+PLAYLIST_URL=$(curl -s -L $STREAM_URL | jq '.hls_mp3_128_url' | sed s/\"//g | xargs -0 echo -E)
 PARTS=$(curl -s -L $PLAYLIST_URL)
 PARTCOUNT=$(echo "$PARTS" | grep -v "#" | wc -l)
 FILEPARTS=$(echo "$PARTS" | grep -v "#" | tr '\n' ' ')
@@ -23,5 +35,7 @@ rm -rf "$FOLDER/$TITLE.part"
 rm -rf "$FOLDER/$TITLE.mp3"
 curl -s -L $FILEPARTS >> "$FOLDER/$TITLE.part"
 mv "$FOLDER/$TITLE.part" "$FOLDER/$TITLE.mp3"
+
+id3v2 -a "$ARTIST" -t "$TRACK" "$FOLDER/$TITLE.mp3"
 
 echo "complete: $TITLE ($PARTCOUNT parts)"
